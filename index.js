19e107d127a39a1ee21344ad15da480ebe7dcb20
ashiku2 <ashikur1602@gmail.com>
2024-11-14 21:40:15 +0600

    some changes

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ app.use(express.json());
 // WatchListMaster
 // 5qTdq3ZahsAQEcKv
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bpilnp1.mongodb.net/?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bpilnp1.mongodb.net/?retryWrites=true&w=majority`;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bpilnp1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -30,8 +32,7 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
-        const movieCollection = client.db('movieDB').collection('movie');
-        const cartCollection = client.db('movieDB').collection('cart');
+        const movieCollection = client.db('watchList').collection('cartoons');
 
         app.get('/movie', async (req, res) => {
             const cursor = movieCollection.find();
@@ -83,26 +84,7 @@ async function run() {
             res.send(result);
         })
 
-        // cart related api 
-        app.post('/cart', async (req, res) => {
-            const cartItem = req.body;
-            const result = await cartCollection.insertOne(cartItem);
-            res.send(result);
-        })
-
-        app.get('/cart', async (req, res) => {
-            const cursor = cartCollection.find();
-            const result = await cursor.toArray();
-            res.send(result);
-        })
-
-        app.delete('/cart/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) };
-            const result = await cartCollection.deleteOne(query);
-            res.send(result);
-        })
-
+        
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
